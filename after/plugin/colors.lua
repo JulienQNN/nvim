f5386a57a2e3a94103cf0affc48a87429b80c8a6
JULIEN <julien.quennehen@edu.devinci.fr>
2023-01-16 18:03:23 +0100

    feat: 🎸 tree siter plugin

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -12,5 +12,3 @@ require("catppuccin").setup({
 
 color = color or "catppuccin"
 	vim.cmd.colorscheme(color)
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
