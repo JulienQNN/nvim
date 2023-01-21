cb1a88ffcdc6d90a0b5ff9dda6652d34ac03eb38
JULIEN <julien.quennehen@edu.devinci.fr>
2023-01-21 17:45:47 +0100

    feat: 🎸 remap to next and previous mark for harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -5,5 +5,5 @@ vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 
+vim.keymap.set("n", "<C-g>", function() ui.nav_prev() end)
 vim.keymap.set("n", "<C-h>", function() ui.nav_next() end)
-vim.keymap.set("n", "<C-t>", function() ui.nav_prev() end)
